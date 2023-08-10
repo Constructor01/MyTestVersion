aac05cc6204c117de7843a245d9ec26cb546858c
Constructor01 <103926914+Constructor01@users.noreply.github.com>
2023-08-10 23:26:48 +0300

    Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -24,7 +24,7 @@ private:
     std::mutex mtx;
     std::condition_variable cv;
     bool data_ready;
-    std::string shared_buffer;
+    int sum; // Store the sum calculated in thread1
     std::thread t1, t2;
 
     bool is_all_digits(const std::string& str) {
@@ -46,13 +46,13 @@ private:
                     }
                 }
                 std::replace(input.begin(), input.end(), 'K', 'B');
+                
+                sum = calculate_sum(input); // Calculate sum of original numerical values
 
                 std::unique_lock<std::mutex> lock(mtx);
-                shared_buffer = input;
                 data_ready = true;
                 cv.notify_one();
-            }
-            else {
+            } else {
                 std::cout << "Invalid input. Please enter a valid string of digits." << std::endl;
             }
         }
@@ -61,38 +61,26 @@ private:
     void thread2() {
         while (true) {
             std::unique_lock<std::mutex> lock(mtx);
-            cv.wait(lock, [this] { return data_ready; });
+            cv.wait(lock, [this]{ return data_ready; });
 
-            std::string data = shared_buffer;
+            // Simulate passing the sum to Program #2 via a socket
+            send_sum_to_program2(sum);
+
             data_ready = false;
-            lock.unlock();
-
-            // Simulate passing the data to Program #2 via a socket
-            send_data_to_program2(data);
-
-            std::cout << "Processed data: " << data << std::endl;
-
-            int sum = 0;
-            for (char c : data) {
-                if (isdigit(c)) {
-                    sum += c - '0';
-                }
-            }
-
-            // Simulate passing the sum to Program #2
-            std::cout << "Sum of numerical values: " << sum << std::endl;
-
-            // You can place the code here to pass the sum to Program #2
-
-            // Note: You might want to handle the connection to Program #2 here, so it's not affected
-            // by Program #2 crashes or restarts.
-
-            // In this example, we'll just print a message to simulate the transfer.
-            std::cout << "Sum sent to Program #2." << std::endl;
         }
     }
 
-    void send_data_to_program2(const std::string& data) {
+    int calculate_sum(const std::string& data) {
+        int sum = 0;
+        for (char c : data) {
+            if (isdigit(c)) {
+                sum += c - '0';
+            }
+        }
+        return sum;
+    }
+
+    void send_sum_to_program2(int sum) {
         int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
         if (clientSocket == -1) {
             std::cerr << "Error creating socket" << std::endl;
@@ -110,8 +98,8 @@ private:
             return;
         }
 
-        // Send the data to Program #2
-        send(clientSocket, data.c_str(), data.size(), 0);
+        // Send the sum to Program #2
+        send(clientSocket, &sum, sizeof(sum), 0);
 
         close(clientSocket);
     }
